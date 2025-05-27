ccdc2585c8bbe45d3b143a643fae80bc46cbb9b5
Venkatesh664 <103093498+Venkatesh664@users.noreply.github.com>
2025-05-27 11:54:04 +0530

    Update SpringMysqlEmployeeApplication.java

--- spring-mysql-employee/src/main/java/com/praveen/Employee/SpringMysqlEmployeeApplication.java
+++ spring-mysql-employee/src/main/java/com/praveen/Employee/SpringMysqlEmployeeApplication.java
@@ -1,13 +1,19 @@
-package com.praveen.Employee;
+package com.praveen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringMysqlEmployeeApplication {
+public class SpringMysqlEmployeeApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMysqlEmployeeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMysqlEmployeeApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringMysqlEmployeeApplication.class);
+    }
 }
