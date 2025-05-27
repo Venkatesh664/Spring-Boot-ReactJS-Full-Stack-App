3504b379279acbff08769a5d45fe41885056b380
Venkatesh664 <103093498+Venkatesh664@users.noreply.github.com>
2025-05-27 15:13:18 +0530

    Update SpringMysqlEmployeeApplicationTests.java

--- spring-mysql-employee/src/test/java/com/praveen/Employee/SpringMysqlEmployeeApplicationTests.java
+++ spring-mysql-employee/src/test/java/com/praveen/Employee/SpringMysqlEmployeeApplicationTests.java
@@ -1,19 +1,12 @@
-package com.praveen;
+package com.praveen.Employee;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class SpringMysqlEmployeeApplication extends SpringBootServletInitializer {
+@SpringBootTest
+public class SpringMysqlEmployeeApplicationTests {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringMysqlEmployeeApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(SpringMysqlEmployeeApplication.class);
+    @Test
+    void contextLoads() {
     }
 }
